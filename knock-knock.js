29f44bf6d9a6e6c2c33a23a9c82fa798e5570f5d
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-30 18:44:22 +0000

    created eventemitters and removed event emitters.

---
+++ knock-knock.js
@@ -0,0 +1,29 @@
+const EventEmitter = require('events');
+
+class Emitter extends EventEmitter {}
+emitter = new Emitter();
+
+emitter.on('knock', function() {
+  console.log('Who\'s there?');
+});
+
+emitter.on('knock', function() {
+  console.log('Go away!');
+});
+
+emitter.emit('knock');
+emitter.removeAllListeners();
+emitter.emit('knock');
+
+// const EventEmitter = require('events');
+
+// class Emitter extends EventEmitter {}
+// emitter = new Emitter();
+
+// emitter.once('knock', function() {
+//   console.log('Who\'s there?');
+// });
+
+
+// emitter.emit('knock');
+// emitter.emit('knock');
