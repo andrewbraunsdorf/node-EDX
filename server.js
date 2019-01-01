8df20da9e6eec3f5d5d62c83d3f0f1d7edb6d1e2
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-01 18:00:14 +0000

    ran the file in localhost, opend a new terminal and ran localhost to find the program still running, changed content type to text plain and application/json, by terminating the terminal and restarting the program.

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-const path = require('path');
-const server = require(path.join(__dirname, 'app', 'server.js')) ;
+const http = require('http');
+const port = 3000;
+http.createServer((req, res) => {
+  res.writeHead(300, {'Content-Type': 'application/json'});
+  res.end('Hello World\n');
+}).listen(port);
+
+console.log(`Server running at http://localhost:${port}/`);
+
+//ran the file in localhost, opend a new terminal and ran localhost to find the program still running, changed content type to text plain and application/json, by terminating the terminal and restarting the program.
